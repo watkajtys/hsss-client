8ef0982220d710855a33740af3b335082453fc02
Matt Watkajtys <watkajtys@gmail.com>
2016-06-06 06:01:18 -0400

    Message component

---
+++ src/components/messaging/Message.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+var _ = require('lodash');
+require('../../css/messaging.css');
+
+export default React.createClass({
+  componentWillMount: function () {
+    this.id = _.uniqueId('message_');
+  },
+  messageClass: function() {
+    console.log(this.props);
+    let sender = this.props.sender;
+    return 'message ' + sender;
+  },
+  render : function () {
+    return (
+      <div className={this.messageClass()} id={this.id}>
+        <p>{this.props.msg.content}</p>
+      </div>
+    )
+  }
+});
