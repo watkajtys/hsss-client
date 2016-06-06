b523eada13cbfa8820ff284f2d14948f1aa8a917
Matt Watkajtys <watkajtys@gmail.com>
2016-06-06 09:48:11 -0400

    trifurcate type for slides

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import Messaging from './Messaging';
 import Splash from './intro/Splash';
 import Intro from './intro/Intro';
+import EmbeddedSlider from './EmbeddedSlider';
 let _ = require('lodash');
 
 export default React.createClass({
   componentWillMount: function () {
     this.id = _.uniqueId('side_');
-  },
-  componentDidUpdate: function () {
-    console.log(this.props.activeSlide, 'ACTIVE FROM SLIDE')
   },
   isSplash: function (slide) {
     if (slide.special && slide.specialType === 'splash') {
@@ -21,7 +19,13 @@ export default React.createClass({
       return true
     }
   },
+  trifurcate : function (slide) {
+    if (slide.special && slide.specialType === 'trifurcate') {
+      return true
+    }
+  },
   deckIsActive: function (slide) {
+    console.log(slide, "DECK SLIDE");
     if (this.props.activeSlide == slide.deck) {
       return true
     }
@@ -31,9 +35,11 @@ export default React.createClass({
     if (this.isSplash(this.props.slide)) {
       slideType = <Splash />;
     } else if (this.isIntro(this.props.slide)) {
-      slideType = <Intro deck={this.props.slide} active={this.deckIsActive(this.props.slide)}/>;
+      slideType = <Intro deck={this.props.slide} activeSlide={this.props.activeSlideVert} active={this.deckIsActive(this.props.slide)}/>;
+    } else if (this.trifurcate(this.props.slide)) {
+      slideType = <EmbeddedSlider slides={this.props.slide.sections} activeVert={this.props.activeSlideVert} initial="1" class=".swiper-container-hor" deck={this.props} active={this.deckIsActive(this.props.slide)}/>
     } else {
-      slideType = <Messaging deck={this.props.slide} active={this.deckIsActive(this.props.slide)}/>;
+      slideType = <Messaging deck={this.props.slide} activeVert={this.props.activeSlide} active={this.deckIsActive(this.props.slide)}/>;
     }
     return (
       <div className="slide swiper-slide" key={this.id}>
