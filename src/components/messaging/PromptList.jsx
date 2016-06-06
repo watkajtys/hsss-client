79276a3adf3a248b6d546b7c7011d9a376052c22
Matt Watkajtys <watkajtys@gmail.com>
2016-06-06 06:01:51 -0400

    prompt list

---
+++ src/components/messaging/PromptList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+var _ = require('lodash');
+
+export default React.createClass({
+  componentWillMount: function () {
+    this._id = _.uniqueId('prompt-list_');
+  },
+  componentDidMount: function () {
+    console.log(this.props.messages, 'MOUNTED');
+  },
+  render : function () {
+    var that = this;
+    console.log(that, 'THAT');
+    return (
+      <div className="prompt-line">
+        {that.props.prompts.map(prompt =>
+          <div className="prompt" onClick={ () => {that.props.addMessage(prompt)}}><p>{prompt.prompt}</p></div>
+        )}
+      </div>
+    )
+  }
+})
