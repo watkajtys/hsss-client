d892abb1a4cc210622cea125f82867e179ee0c7e
Matt Watkajtys <watkajtys@gmail.com>
2016-06-06 02:45:28 -0400

    minor script tweaks to better handle the text blocks (now an array of arrays)

--- src/script/shared.js
+++ src/script/shared.js
@@ -1,5 +1,5 @@
 let intro_script = [{
-  deck        : '1',
+  deck        : '0',
   special     : true,
   specialType : 'splash',
   messages    : [
@@ -9,29 +9,16 @@ let intro_script = [{
     }
   ]
 },
-
   {
-    deck            : '2',
-    messages        : [
+    deck             : '1',
+    messages         : [
       {
         sender  : 'narrator',
         content : 'Hello!'
       }
     ],
-    reactionType    : 'buttons',
-    reactionOptions : [
-      {
-        prompt     : 'Hello?',
-        deckLoad   : true,
-        deckAction : '2_a'
-      }
-    ]
-  },
-  {
-    deck            : '2_a',
-    tense           : 'present',
-    description     : 'Narrator Introduction Part2',
-    messages        : [
+    messageFollowUps : true,
+    messageFollowUp  : [
       {
         sender  : 'narrator',
         content : 'Who am I you must be wondering...'
@@ -57,48 +44,77 @@ let intro_script = [{
         content : "Let's get started, shall we?"
       }
     ],
-    reactionType    : 'buttons',
-    reactionOptions : [
+    reaction         : true,
+    reactionType     : 'buttons',
+    reactionOptions  : [
       {
-        prompt     : 'Ok!',
-        deckLoad   : true,
-        deckAction : '3'
+        prompt         : 'Hello?',
+        deckLoad       : false,
+        routeLoad      : false,
+        reactionType   : 'buttons',
+        promptFollowUp : [
+          {
+            prompt     : 'Ok!',
+            deckLoad   : true,
+            deckAction : '3'
+          }
+        ]
       }
     ]
   },
   {
-    deck            : '3',
-    tense           : 'present',
-    description     : 'Character Introduction',
-    special         : true,
-    specialType     : 'takeovers',
-    takeover        : [
-      {
-        sender  : 'narrator',
-        content : "Between what is said and not meant, and what is meant and not said, most of love is lost."
-      },
-      {
-        sender  : 'narrator',
-        content : "John, a tech entrepreneur, met Sue, a freelance writer, at a Bushwick rave."
-      },
-      {
-        sender  : 'narrator',
-        content : "At the time of meeting, they were in an open relationship with other people."
-      }
-    ],
-    reactionType    : 'buttons',
-    reactionOptions : [
-      {
-        prompt     : 'He',
-        deckLoad   : true,
-        deckAction : '4'
-      },
-      {
-        prompt      : "She",
-        deckLoad    : false,
-        routeLoad   : true,
-        routeAction : '4S'
-      }
+    deck        : '2',
+    tense       : 'present',
+    description : 'Character Introduction',
+    special     : true,
+    specialType : 'intro',
+    textBlock   : [
+      [
+        {
+          sender  : 'narrator',
+          content : '"Between what is said and not meant,',
+          id      : 'introMsg1'
+        },
+        {
+          sender  : 'narrator',
+          content : 'and what is meant and not said,',
+          id      : 'introMsg2'
+        },
+        {
+          sender  : 'narrator',
+          content : 'most of love is lost."',
+          id      : 'introMsg3'
+        }
+      ],
+      [
+        {
+          sender  : 'narrator',
+          content : "JOHN, a tech entrepreneur met",
+          avatar  : 'john',
+          id      : 'introMsg4'
+
+        },
+        {
+          sender  : 'narrator',
+          content : "SUE, a freelance writer, at a Bushwich rave.",
+          avatar  : 'sue',
+          id      : 'introMsg5'
+        }
+      ],
+      [
+        {
+          sender  : 'narrator',
+          content : 'At the time of meeting, they were in an open relationship with other people.',
+          id      : 'introMsg6'
+        }
+      ],
+      [
+        {
+          sender: 'narrator',
+          content: 'Whose point of view do you want to experience first?',
+          swipe: true
+        }
+      ]
     ]
   }
 ];
