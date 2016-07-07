900cbf0dca289c96696213a9ed827d800d153587
Matt Watkajtys <watkajtys@gmail.com>
2016-07-06 23:20:21 -0400

    messaging rewrite in progress to handle advanced timing

--- src/components/Messaging.jsx
+++ src/components/Messaging.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import assign from 'deep-assign';
 import Message from './messaging/Message';
 import PromptList from './messaging/PromptList';
 
@@ -31,7 +32,7 @@ export default React.createClass({
     return this.props.deck.reaction;
   },
   getPrompts : function () {
-    console.log('%cCALLED', 'color: red; font-size: 16px');
+    console.log('%cCALLED Prompts', 'color: red; font-size: 16px');
     let prompts;
     let that = this;
     prompts = this.props.deck.reactionOptions || [];
@@ -45,63 +46,117 @@ export default React.createClass({
     let display = [];
     let displayInterval = 2500;
     messages     = this.props.deck.messages || null;
-    // if (this.props.deck.messageFollowUps) {
-    //   messages = this.props.deck.messageFollowUp;
-    // } else {
-    //   messages = this.props.deck.messages || [];
-    // }
-    console.log(messages, 'messages');
+
+
     let that = this;
     var count=0;
     if (messages) {
-      let timer = setInterval(function() {
-        console.log('%cStarted %s deck is active', 'color: yellow; background: black;', that.props.deck.deck);
-        let obj = messages[count];
-        that.setState({data: that.state.data.concat([obj])});
-        count++;
-        console.log(count, messages.length);
-        if (count >= messages.length) {
-          clearInterval(timer);
-          setTimeout(function() {
-            that.getPrompts();
-          }, displayInterval);
-        }
-      }, displayInterval);
+      // console.log(messages, messages.length);
+      // //FIRST RUN - POST FIRST MESSAGE WITHOUT DELAY
+      // let obj = messages[count];
+      // that.setState({data: that.state.data.concat([obj])});
+      // if (count < messages.length) {
+      //
+      // } else {
+      //   that.getPrompts();
+      // }
+      this.executeMessaging(messages);
+      // let timer = setInterval(function() {
+      //   console.log('%cStarted %s deck is active', 'color: yellow; background: black;', that.props.deck.deck);
+      //   let obj = messages[count];
+      //   that.setState({data: that.state.data.concat([obj])});
+      //   count++;
+      //   console.log(count, messages.length);
+      //   if (count >= messages.length) {
+      //     clearInterval(timer);
+      //     setTimeout(function() {
+      //       that.getPrompts();
+      //     }, displayInterval);
+      //   }
+      // }, displayInterval);
     }
   },
+  runMessagingPromise : function (messageArray) {
+    var deferred = $.Deferred();
+
+    var i        = 0;
+    var that     = this;
+    var nextStep = function () {
+      if (i < messageArray.length) {
+        // Do something
+        var delay = messageArray[i].delay ? messageArray[i].delay : 2000;
+        let obj   = messageArray[i];
+        obj.last  = true;
+        obj.delay = delay;
+        that.setState({data : that.state.data.concat([obj])});
+        setTimeout(nextStep, delay);
+        i++;
+      }
+      else {
+        deferred.resolve(i);
+      }
+    };
+    nextStep();
+    return deferred.promise();
+  },
+  executeMessaging : function(messageArray, prompts) {
+      var that = this;
+      var promise = this.runMessagingPromise(messageArray);
+      promise.then(function(result) {
+        if (prompts)  {
+          that.setState({prompts: [{prompt: prompts}]});
+        } else {
+          that.getPrompts();
+        }
+      });
+  },
   addMessage : function(message) {
-    let that = this;
+    console.log(message, "MESSAGE TO ADD");
     this.setState({prompts: []});
+    let that = this;
     if (message.prompt) {
       let obj = {
         sender: 'user',
         content: message.prompt,
         skipDelay: true
       };
-      that.setState({data: that.state.data.concat([obj])});
-    }
-    if (message.loadMore) {
+      this.setState({data: this.state.data.concat([obj])});
 
-      var count=0;
-      if (message.messagesToLoad) {
-        let timer2 = setInterval(function() {
-          console.log('%cStarted %s deck is active', 'color: yellow; background: black;', that.props.deck.deck);
-          let obj = message.messagesToLoad[count];
-          that.setState({data: that.state.data.concat([obj])});
-          count++;
-          console.log(count, message.messagesToLoad.length);
-          if (count >= message.messagesToLoad.length) {
-            console.log('%cSHOULD NUKE at %d', 'color:orange', count);
-            clearInterval(timer2);
-            setTimeout(function() {
-              if (message.additionalPrompt) {
-                that.setState({prompts: [{prompt: message.promptFollowUp[0].prompt}]});
-              }
-            }, 2500);
-          }
-        }, 2500);
+      if (message.loadMore) {
+        if (message.messagesToLoad) {
+          setTimeout(function () {
+            if (message.additionalPrompt) {
+              that.executeMessaging(message.messagesToLoad, message.promptFollowUp[0].prompt);
+            } else {
+              that.executeMessaging(message.messagesToLoad);
+            }
+
+          }, 500);
+        }
       }
     }
+
+
+      // var count=0;
+      // if (message.messagesToLoad) {
+      //   let timer2 = setInterval(function() {
+      //     console.log('%cStarted %s deck is active', 'color: yellow; background: black;', that.props.deck.deck);
+      //     let obj = message.messagesToLoad[count];
+      //     that.setState({data: that.state.data.concat([obj])});
+      //     count++;
+      //     console.log(count, message.messagesToLoad.length);
+      //     if (count >= message.messagesToLoad.length) {
+      //       console.log('%cSHOULD NUKE at %d', 'color:orange', count);
+      //       clearInterval(timer2);
+      //       setTimeout(function() {
+      //         if (message.additionalPrompt) {
+      //           that.setState({prompts: [{prompt: message.promptFollowUp[0].prompt}]});
+      //         }
+      //       }, 2500);
+      //     }
+      //   }, 2500);
+      // }
+
     // this.setState({data: this.state.data.concat([message])});
   },
   messageClass : function () {
