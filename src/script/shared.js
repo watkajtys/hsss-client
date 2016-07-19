ea8dadcc05ca890c591f2999d75a09314c473bf7
Matt Watkajtys <watkajtys@gmail.com>
2016-07-19 03:05:22 -0400

    shared.js script updates [moving john and sue to new files -- will be different commit]

--- src/script/shared.js
+++ src/script/shared.js
@@ -1,17 +1,23 @@
+import sue_slides from './sue';
+import john_slides from './john'
+
 let intro_script = [{
-  deck        : '0',
-  description : 'start',
-  special     : true,
-  specialType : 'splash',
-  messages    : [
-    {
-      sender  : 'narrator',
-      content : 'An interactive story about dating in the digital age'
-    }
-  ]
-},
+    deck        : '0',
+    slide       : 'D1',
+    description : 'start',
+    special     : true,
+    specialType : 'splash',
+    loadNextAutomatically: true,
+    messages    : [
+      {
+        sender  : 'narrator',
+        content : 'An interactive story about dating in the digital age'
+      }
+    ]
+  },
   {
     deck            : '1',
+    slide           : 'D2.1',
     description     : 'hello',
     messages        : [
       {
@@ -56,8 +62,8 @@ let intro_script = [{
         promptFollowUp   : [
           {
             prompt     : 'Ok!',
-            deckLoad   : true,
-            deckAction : '3'
+            slideLoad   : true,
+            slideToLoad : 'D2.2'
           }
         ]
       }
@@ -66,11 +72,14 @@ let intro_script = [{
   {
     container   : 'story',
     description : 'story-container',
+    slide       : 'D2.2',
+    episodeToStart: 'D3',
     deck        : '2',
     order       : 1,
     tense       : 'present',
     special     : true,
     specialType : 'intro',
+    loadNextAutomatically: true,
     textBlock   : [
       [
         {
@@ -125,193 +134,17 @@ let intro_script = [{
     tense       : 'present',
     special     : true,
     specialType : 'bifurcate',
+    slide       : 'D3',
     sections    : [
       {
         container       : 'john',
         description     : 'john-container',
-        order           : 0,
-        characterSlides : [
-          {
-            slideDeck       : '3',
-            charmsg         : 'he',
-            messages        : [
-              {
-                sender  : 'narrator',
-                content : "Okay...Here is John's side of the story."
-              },
-              {
-                sender  : 'narrator',
-                content : "(You can switch to Sue's perspective at any point by swiping left)"
-              }
-            ],
-            reaction        : true,
-            reactionType    : 'buttons',
-            reactionOptions : [
-              {
-                prompt       : 'Thumbs Up',
-                deckLoad     : true,
-                reactionType : 'buttons'
-              }
-            ]
-          },
-          {
-            slideDeck   : '4',
-            description : 'john-number-exchange',
-            special     : true,
-            specialType : 'audio',
-            header      : 'Number Exchange',
-            audioFile   : 'file',
-            gender      : 'he'
-          }
-        ]
+        characterSlides : john_slides
       },
       {
         container       : 'sue',
         description     : 'sue-container',
-        order           : 2,
-        characterSlides : [
-          {
-            charmsg         : 'she',
-            slideDeck       : '3',
-            slideOrder      : 2,
-            description     : 'okay-sues-side',
-            messages        : [
-              {
-                sender  : 'narrator',
-                content : "Howdy!",
-                delay   : 1000
-              },
-              {
-                sender  : 'narrator',
-                content : "Here is my side of the story",
-                delay   : 500
-              },
-              {
-                sender  : 'narrator',
-                content : "(You can switch to John's side at any point by swiping right)",
-                delay   : 1000
-              }
-            ],
-            reaction        : true,
-            reactionType    : 'buttons',
-            reactionOptions : [
-              {
-                prompt         : 'Thumbs Up',
-                deckLoad       : true,
-                reactionType   : 'buttons',
-                loadMore       : true,
-                messagesToLoad : [
-                  {
-                    sender  : 'narrator',
-                    content : "Sue?"
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            slideDeck   : '4',
-            description : 'sues-number-exchange',
-            special     : true,
-            specialType : 'audio',
-            header      : 'Number Exchange!',
-            audioFile   : 'file',
-            gender      : 'she'
-          },
-          {
-            deck        : '6',
-            description : 'sue-and-john-exchange',
-            charmsg     : 'she',
-            messages    : [
-              {
-                sender        : 'john',
-                content       : 'Sue',
-                skipDelay     : true,
-                delay         : 1000,
-                displayAvatar : true
-              },
-              {
-                sender        : 'sue',
-                content       : 'John',
-                delay         : 1500,
-                displayAvatar : true
-              },
-              {
-                sender         : 'sue',
-                content        : 'Jon?',
-                delay          : 1500,
-                lastMsgInBlock : true
-              },
-              {
-                sender        : 'john',
-                content       : 'Ha. John. If I don\'t see you again for the rest of the night, your lips will still be the only thing on my mind',
-                delay         : 1500,
-                displayAvatar : true
-              }
-            ]
-          },
-          {
-            deck            : '7',
-            description     : 'hello2',
-            messages        : [
-              {
-                sender    : 'narrator',
-                content   : 'By the way, I should mention everything here is collected from a couple\'s text history, documenting their first 48 hours of courtship.',
-                skipDelay : true,
-                delay     : 1000
-              },
-              {
-                sender    : 'narrator',
-                content   : 'Their names have been changed of course.',
-                delay     : 1000
-              },
-              {
-                sender    : 'narrator',
-                content   : 'Shall we continue?',
-                delay     : 1000
-              }
-            ],
-            reaction        : true,
-            reactionType    : 'buttons',
-            reactionOptions : [
-              {
-                prompt           : 'Yes Please!',
-                deckLoad         : false,
-                routeLoad        : false,
-                reactionType     : 'buttons',
-                loadMore         : true,
-                messagesToLoad   : [
-                  {
-                    sender  : 'narrator',
-                    content : "Back to Sue then..."
-                  }
-                ]
-              },
-              {
-                prompt           : 'No wait!',
-                deckLoad         : false,
-                routeLoad        : false,
-                reactionType     : 'buttons',
-                loadMore         : true,
-                messagesToLoad   : [
-                  {
-                    sender  : 'narrator',
-                    content : "So you're a context person, huh?"
-                  }
-                ],
-                additionalPrompt : true,
-                promptFollowUp   : [
-                  {
-                    prompt     : 'Up'
-                  },
-                  {
-                    prompt : 'Down'
-                  }
-                ]
-              }
-            ]
-          }
-        ]
+        characterSlides : sue_slides
       }
     ]
   }
