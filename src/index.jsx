19e648566789b6992ed5e79ac3130dbff532d37f
Matt Watkajtys <watkajtys@gmail.com>
2016-07-19 02:59:53 -0400

    removing unused router and just using provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory} from 'react-router';
+import { Provider } from  'react-redux';
+import store from './components/shared/store.js';
 import App from './components/App';
 
 require('./css/style.css');
 
-const routes = <Route component={App}>
-  <Route path="/" component={App} />
-</Route>;
-
 ReactDOM.render(
-  <Router history={hashHistory}>{routes}</Router>,
+  <Provider store={store}><App/></Provider>,
   document.getElementById('app')
 );
