4958bbf10b9e580d70d1c366c7e108cb3bd1cedb
Matt Watkajtys <watkajtys@gmail.com>
2016-08-02 00:10:17 -0400

    splash style updates

--- src/components/intro/Splash.jsx
+++ src/components/intro/Splash.jsx
@@ -6,20 +6,12 @@ export default React.createClass({
     return (
       <div id="splash">
         <div className="splash-logo-wrapper">
-          <div className="he-wrap">
-            <h1>He Said...</h1>
-          </div>
-          <div className="she-wrap">
-            <h1>She Said</h1>
-          </div>
+          <div className="logo-container"></div>
           <h3 className="center-text">
             An interactive story about dating in the digital age.
           </h3>
         </div>
-        <div className="start-btn">
-          <i className="fa fa-angle-up up-icon"></i>
-          <h1 className="start-text">Start</h1>
-        </div>
+        <div className="start-btn"></div>
       </div>
     )
   }
