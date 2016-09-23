5a82dce0af519a95032cc3f754c75cc336a19814
Matt Watkajtys <watkajtys@gmail.com>
2016-09-23 11:27:36 -0400

    lockedSide in deck reducer

--- src/components/shared/reducers/deck-reducer.js
+++ src/components/shared/reducers/deck-reducer.js
@@ -2,15 +2,17 @@ import * as types from '../actions/action-types.js';
 
 const initialState = {
   activeSlide     : 'D1',
-  activeContainer : 'INTRO'
+  activeContainer : 'INTRO',
+  lockedSide      : false
 };
 
 const deckReducer = function (state = initialState, action) {
   switch (action.type) {
     case types.CHANGE_SLIDE :
       return Object.assign({}, state, {
-        activeSlide : action.activeSlide,
-        activeParent: action.activeParent
+        activeSlide  : action.activeSlide,
+        activeParent : action.activeParent,
+        lockedSide   : action.lockedSide
       });
     case types.UPDATE_ACTIVE_CONTAINER :
       return Object.assign({}, state, {
