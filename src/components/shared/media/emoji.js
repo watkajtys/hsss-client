28d25494e1780c0509365da9d6be0ee7761457cd
Matt Watkajtys <watkajtys@gmail.com>
2016-10-05 02:12:14 -0400

    adding new emojis to emoji assigner

--- src/components/shared/media/emoji.js
+++ src/components/shared/media/emoji.js
@@ -8,6 +8,17 @@ var interrobang   = require('../../../images/emoji/interrobang.png');
 var celebration   = require('../../../images/emoji/celebration.png');
 var appreciation  = require('../../../images/emoji/appreciation.png');
 var hundred       = require('../../../images/emoji/hundred.png');
+var fuego         = require('../../../images/emoji/fuego.png');
+var broken_heart  = require('../../../images/emoji/broken_heart.png');
+var heart         = require('../../../images/emoji/heart.png');
+var okay          = require('../../../images/emoji/okay.png');
+var hug           = require('../../../images/emoji/hug.png');
+var love_eyes     = require('../../../images/emoji/love_eyes.png');
+var o_face        = require('../../../images/emoji/o_face.png');
+var bolt_face     = require('../../../images/emoji/bolt_face.png');
+var tear          = require('../../../images/emoji/tear.png');
+var super_happy   = require('../../../images/emoji/super_happy.png');
+var enter         = require('../../../images/emoji/enter.png');
 
 function emojiAssign(emoji) {
   switch (emoji) {
@@ -15,14 +26,20 @@ function emojiAssign(emoji) {
       return thumbs_up;
     case 'thumbs_down' :
       return thumbs_down;
+    case 'okay' :
+      return okay;
     case 'shitty' :
       return shitty;
+    case 'fuego' :
+      return fuego;
     case 'question' :
       return question;
     case 'nope' :
       return nope;
     case 'love_eyes_cat':
       return love_eyes_cat;
+    case 'love_eyes' :
+      return love_eyes;
     case 'interrobang' :
       return interrobang;
     case 'celebration' :
@@ -31,6 +48,22 @@ function emojiAssign(emoji) {
       return appreciation;
     case 'hundred' :
       return hundred;
+    case 'heart' :
+      return heart;
+    case 'broken_heart':
+      return broken_heart;
+    case 'hug' :
+      return hug;
+    case 'o_face' :
+      return o_face;
+    case 'bolt_face':
+      return bolt_face;
+    case 'tear':
+      return tear;
+    case 'super_happy' :
+      return super_happy;
+    case 'enter' :
+      return enter;
     default :
       return thumbs_up
   }
