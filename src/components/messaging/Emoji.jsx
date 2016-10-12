6b5115d26ba3fe435f46fcdd62cd5a910bcb2086
Matt Watkajtys <watkajtys@gmail.com>
2016-10-11 22:27:20 -0400

    emoji handling alreadyselected class additions and click processing

--- src/components/messaging/Emoji.jsx
+++ src/components/messaging/Emoji.jsx
@@ -1,22 +1,52 @@
 import React from 'react';
 import {emojiAssign} from '../shared/media/emoji';
 var _ = require('lodash');
+var classNames = require('classnames');
 
 
 export default React.createClass({
-  render : function () {
-    let promptContent;
-    let emoji = emojiAssign(this.props.emoji.emoji);
-    promptContent = <p><img className="emoji" src={emoji} alt={this.props.emoji.emoji}/></p>;
 
-    var messageToAdd = {
+  getInitialState : function () {
+    return {
+      disabled : false
+    }
+  },
+
+  componentWillMount : function () {
+    this.messageToAdd = {
       emojiboard : true,
       emoji : this.props.emoji.emoji,
       value : this.props.emoji.value
     };
 
+    this.emoji = emojiAssign(this.props.emoji.emoji);
+  },
+
+  processClick : function (func) {
+    if (this.messageToAdd.emoji === 'enter') {
+      //IF IT'S THE ENTER BUTTON
+      this.messageToAdd.enterButton = true;
+      func(this.messageToAdd);
+    }
+
+    if (this.props.allowSelection && !this.state.disabled) {
+      //IF THE EMOJI CAN BE CLICKED AND EMOJI BOARD ALLOWS IT
+      func(this.messageToAdd);
+      this.setState({disabled : true});
+    }
+  },
+
+  render : function () {
+    var emojiClass = classNames({
+      'prompt': true,
+      'emoji': true,
+      'alreadyselected': this.state.disabled
+    });
+    let promptContent;
+    promptContent = <p><img className="emoji" src={this.emoji} alt={this.props.emoji.emoji}/></p>;
+
     return (
-      <div className="prompt emoji" onClick={ () => {this.props.addMessage(messageToAdd)}}>{promptContent}</div>
+      <div className={emojiClass} onClick={ () => {this.processClick(this.props.addMessage)}}>{promptContent}</div>
     )
   }
 })
