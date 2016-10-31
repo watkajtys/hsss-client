6a51b98c78dbac0ae3e6a4e4d674072f63a8cf67
Matt Watkajtys <watkajtys@gmail.com>
2016-10-31 17:26:23 -0400

    adjusting store for devtools use

--- src/components/shared/store.js
+++ src/components/shared/store.js
@@ -2,5 +2,5 @@ import { createStore } from 'redux';
 import reducers from './reducers/reducers.js';
 
 // const store = createStore(reducers);
-const store = createStore(reducers, window.devToolsExtension && window.devToolsExtension());
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 export default store;
