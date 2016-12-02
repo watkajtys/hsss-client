0a7efb265e0b8a29fc26fc6735a5414a256b5678
Matt Watkajtys <watkajtys@gmail.com>
2016-12-02 00:02:45 -0500

    update to launch store to know when launch triggered

--- src/components/shared/reducers/episode-reducer.js
+++ src/components/shared/reducers/episode-reducer.js
@@ -2,7 +2,8 @@ import * as types from '../actions/action-types';
 
 const initialState = {
   episode    : '1',
-  launchSide : 'SHE'
+  launchSide : 'SHE',
+  launched   : false
 };
 
 const episodeReducer = function (state = initialState, action) {
@@ -10,7 +11,8 @@ const episodeReducer = function (state = initialState, action) {
     case types.LAUNCH_EPISODE :
       return Object.assign({}, state, {
         episode    : action.episode,
-        launchSide : action.launchSide
+        launchSide : action.launchSide,
+        launched   : action.launched
       })
   }
   return state;
